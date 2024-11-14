45aa9ca014b667524e3d6bd6ed18d207368145db
Josue Cayotopa <josuecayotopatamay@gmail.com>
2024-11-13 21:37:56 -0500

    tabla empresa

---
+++ database/migrations/2024_11_14_021736_create_empresas_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('empresas', function (Blueprint $table) {      
+            $table->id();  
+            $table->string('tipo_actividad')->nullable();
+            $table->string('razon_social', 55);
+            $table->string('ruc', 11)->unique();
+            $table->string('nombre_comercial', 255)->nullable();
+            $table->string('numero_decreto', 10)->nullable();
+            $table->string('logo', 255)->nullable();
+            $table->string('email', 100)->nullable();
+            $table->string('telefono', 9)->nullable();
+            $table->string('direccion', 255);
+            $table->string('moneda')->nullable();
+            $table->string('mensaje')->nullable();
+            $table->string('mision')->nullable();
+            $table->string('vision')->nullable();
+            $table->string('facebook')->nullable();
+            $table->string('youtube')->nullable();
+            $table->string('whatsapp')->nullable();
+            $table->boolean('estado')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('empresas');
+    }
+};
